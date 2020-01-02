4b4530f4249c1581efc100f27e8d0f2d1442adcf
207ashu <207ashu@gmail.com>
2020-01-03 01:11:49 +0530

    setting up route,model, and controller

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// initialize our express app
-const app = express()
+const product = require('./routes/product.route'); // Imports routes for the products
+const app = express();
+app.use('/products', product);
 let port = 3000;
-
 app.listen(port, () => {
-    console.log('Server is up and running on port numner ' + port);
+    console.log('Server is up and running on port number ' + port);
 });
