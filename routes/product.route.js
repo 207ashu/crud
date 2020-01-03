85fa70148106662efb08638547ebe38b3041625c
207ashu <207ashu@gmail.com>
2020-01-03 20:42:40 +0530

    configure end points in routes

--- routes/product.route.js
+++ routes/product.route.js
@@ -3,8 +3,13 @@ const router = express.Router();
 
 // Require the controllers WHICH WE DID NOT CREATE YET!!
 const product_controller = require('../controllers/product.controller');
+router.post('/create', product_controller.product_create);
 
+router.get('/:id', product_controller.product_details);
 
+router.put('/:id/update', product_controller.product_update);
+
+router.delete('/:id/delete', product_controller.product_delete);
 // a simple test url to check that all of our files are communicating correctly.
 router.get('/test', product_controller.test);
 module.exports = router;
